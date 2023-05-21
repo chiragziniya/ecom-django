fcc7248dca5628bda28c6fd10971cab6eb4d745e
unknown <chiragziniya@gmail.com>
2023-05-21 23:04:34 +0530

    search pagination

--- carts/urls.py
+++ carts/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('minus_cart/<int:product_id>/', views.minus_cart, name='minus_cart'),
+    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
 ]
 
 # Register your models here.

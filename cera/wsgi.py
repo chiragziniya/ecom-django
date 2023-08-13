8283130508be5eec7128fb03689909ec3d73f65b
chiragziniya <chiragziniya@gmail.com>
2023-08-14 03:04:15 +0530

    Update wsgi.py

--- cera/wsgi.py
+++ cera/wsgi.py
@@ -7,12 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cera.settings')
+
+# application = get_wsgi_application()
+
+# app = application
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cera.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
